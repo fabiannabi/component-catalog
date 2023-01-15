19b575ec67c5ca4b0fb8f59a9f12aa3c9ec21c78
fabiannabi <fabiannabi@yahoo.com.mx>
2023-01-15 16:50:17 -0600

    Sass setup

---
+++ src/pages/components/Cards.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Cards = () => {
+  return (
+    <div>
+      <h1 className="cards-catalog--title">Cards Catalog</h1>
+      <div className="test">hello im here</div>
+    </div>
+  );
+};
+
+export default Cards;
